971220bffd1a07372db24601f7207d982eeaf258
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-04 23:29:39 +0900

    Java setting

---
+++ src/main/java/com/example/javabible/JavaBibleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBibleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javabible/JavaBibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBibleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
